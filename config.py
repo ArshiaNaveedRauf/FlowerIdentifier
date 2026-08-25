64626fc945165c3a62423237d22d7928aaefb654
ArshiaNaveedRauf <arshianaveed0786@gmail.com>
2026-08-25 17:54:09 +0500

    augmentation

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ validation_spl= 0.2
 img_height=224
 img_width=224
 batch_size=32
+factor= (-0.2,0.3)
--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,7 @@
 from src.data.data_loader import DataLoader
 from src.visualization.raw_data_visualizer import RawDataVisualizer
+from src.visualization.augmented_data_visualizer import AugmentedDataVisulaizer
+from src.data.data_augmenter import DataAugmenter
 from config import validation_spl, batch_size,img_width,img_height
 
 
@@ -7,6 +9,8 @@ class FlowerRecognitionPipeline:
     def __init__(self):
         self.loader = DataLoader(validation_spl,img_height,img_width,batch_size)
         self.raw_visualizer= RawDataVisualizer()
+        self.augmenter= DataAugmenter()
+        self.augmentation_visulizer= AugmentedDataVisulaizer()
 
 
 
@@ -14,4 +18,5 @@ class FlowerRecognitionPipeline:
         training_data= self.loader.training_dataset_loader()
         validation_data= self.loader.validation_dataset_loader()
         self.raw_visualizer.raw_data_visualizer(training_data)
+        self.augmentation_visulizer.augmented_data_visualizer(training_data,self.augmenter.augmentation)
         
