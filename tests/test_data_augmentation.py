fd09ab39820df5563108acf9dd3367760b17bc5e
ArshiaNaveedRauf <arshianaveed0786@gmail.com>
2026-08-27 17:29:48 +0500

    fixing test path

--- tests/test_data_augmentation.py
+++ tests/test_data_augmentation.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 
 def test_data_augmentation():
-    image= tf.keras.utils.load_img('/Users/arshianaveed/FlowerRecognition/flower.avif')
+    image= tf.keras.utils.load_img('tests/fixtures/flower.avif')
     image = tf.keras.utils.img_to_array(image)
     image= tf.expand_dims(image, axis=0)
     augmenter =  DataAugmenter()
